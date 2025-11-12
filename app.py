51405f2552163968d4cd9ac7b9e22381a65a0130
keerthanag3106 <gkeerthana2006@gmail.com>
2025-11-12 13:35:34 +0000

    added files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import flask,render_template
+app=flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+if __name__=="__main__"
+    app.run(debug=true)
